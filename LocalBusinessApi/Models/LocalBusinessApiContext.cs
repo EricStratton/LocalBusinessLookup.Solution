0e248b18a3dd00a29349e77753879985a771cd29
Eric Stratton <strattonericj@gmail.com>
2021-01-22 09:28:18 -0800

    add resturant seeded data

--- LocalBusinessApi/Models/LocalBusinessApiContext.cs
+++ LocalBusinessApi/Models/LocalBusinessApiContext.cs
@@ -21,9 +21,17 @@ namespace LocalBusinessApi.Models
             new Shop { ShopId = 3, Name = "Les Fluers", StreetAddress = "1300 Rue St Jean", KeyBusiness = "Plant Shop"},
             new Shop { ShopId = 4, Name = "Auntie A's Antiques", StreetAddress = "44 Junk St", KeyBusiness = "Antiques"},
             new Shop { ShopId = 5, Name = "General Store", StreetAddress = "15 Old Town Ave", KeyBusiness = "Convenience Store"}
-          )
+          );
+
+      builder.Entity<Resturant>()
+          .HasData(
+            new Resturant { ResturantId = 1, Name = "24hr Kabab", StreetAddress = "151 High St", KeyBusiness = "Late night bites"},
+            new Resturant { ResturantId = 2, Name = "Maria's Cantina", StreetAddress = "132 High St", KeyBusiness = "Casual drinks & street food"},
+            new Resturant { ResturantId = 3, Name = "Pastini Ristorante", StreetAddress = "222 Old Town Ave", KeyBusiness = "Italian dining"},
+            new Resturant { ResturantId = 4, Name = "O'Leary's Pub", StreetAddress = "100 High St", KeyBusiness = "Beer on tap & pub food"},
+            new Resturant { ResturantId = 5, Name = "Pierre's Bon Cuisine", StreetAddress = "1310 Rue St Jean", KeyBusiness = "French cuisine"}
+          );
     }
 
-    
   }
 }

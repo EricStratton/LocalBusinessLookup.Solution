0b1a1b225d9359d73891ba7cf96921b4de671849
Eric Stratton <strattonericj@gmail.com>
2021-01-22 08:35:51 -0800

    fix missing semicolon

--- LocalBusinessApi/Startup.cs
+++ LocalBusinessApi/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection
+using Microsoft.Extensions.DependencyInjection;
 
 namespace LocalBusinessApi
 {
